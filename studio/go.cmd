507f3ab04a90cc8d4554cc743c3002cef6f515c8
nikos chalikias <nikos.chalikias@gmail.com>
2020-11-13 18:09:43 +0000

    updated readme

---
+++ studio/go.cmd
@@ -0,0 +1,5 @@
+
+rem cd F:\n-blocks\studio\studio2.0_experimental
+python  F:\n-blocks\studio\studio2.0_experimental\nblocksStudio.py  --verbose --nosync --import altium --netlist .\studio.NET --parameters .\studio.csv --output .\export --name .\export\A -f A
+rem cd F:\n-blocks\s-pro-04\studio
+
